3e8dc90da906fa3a8841d170f0ef4935fb80d1ef
SCPC 308 <SCPC308@labs.upds.bo>
2024-02-28 10:48:46 -0400

    primer comentario

---
+++ formvalidation/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/pages/home_page.dart';
+import 'package:formvalidation/src/pages/login_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => const LoginPage(),
+        'home': (BuildContext context) => const Homepage(),
+      },
+    );
+  }
+}
---
+++ formvalidation/lib/src/pages/login_page.dart
@@ -0,0 +1,156 @@
+//import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        _crearFondo(context),
+        _loginForm(context),
+      ],
+    ));
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        SafeArea(
+          child: Container(
+            height: 180.0,
+          ),
+        ),
+        Container(
+          width: size.width * 0.85,
+          margin: EdgeInsets.symmetric(vertical: 60.0),
+          padding: EdgeInsets.symmetric(vertical: 50.0),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3.0,
+                    offset: Offset(0.0, 5.0),
+                    spreadRadius: 3.0)
+              ]),
+          child: Column(
+            children: <Widget>[
+              const Text('Ingreso', style: TextStyle(fontSize: 17.0)),
+              const SizedBox(height: 60.0),
+              _crearEmail(),
+              const SizedBox(height: 30.0),
+              _crearPassword(),
+              const SizedBox(height: 30.0),
+              _crearBoton(),
+              const SizedBox(height: 15.0), // Espaciado adicional
+              _crearRegistroButton(),
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+
+  Widget _crearEmail() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: const TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Correo electrónico',
+        ),
+      ),
+    );
+  }
+
+  Widget _crearPassword() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: const TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+          labelText: 'Contraseña',
+        ),
+      ),
+    );
+  }
+
+  Widget _crearBoton() {
+    return ElevatedButton(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: const Text('Ingresar'),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget _crearRegistroButton() {
+    return TextButton(
+      onPressed: () {
+        // Aquí puedes agregar la lógica para ir a la pantalla de registro
+      },
+      child: const Text(
+        'Registrarse',
+        style:  TextStyle(color: Colors.deepPurple, fontSize: 16.0),
+      ),
+    );
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: <Color>[
+            Color.fromRGBO(63, 63, 156, 1.0),
+            Color.fromRGBO(90, 70, 178, 1.0)
+          ],
+          )),
+    );
+
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: const Color.fromRGBO(255, 255, 255, 0.05)),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondoMorado,
+        Positioned(top: 90.0, left: 30.0, child: circulo),
+        Positioned(top: -40.0, right: -30.0, child: circulo),
+        Positioned(bottom: -50.0, right: -10.0, child: circulo),
+        Positioned(bottom: 120.0, right: 20.0, child: circulo),
+        Positioned(bottom: -50.0, left: -20.0, child: circulo),
+
+        // ignore: avoid_unnecessary_containers
+        Container(
+          padding: const EdgeInsets.only(top: 80.0),
+          child: const Column(
+            children: <Widget>[
+              Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
+              SizedBox(height: 10.0, width: double.infinity),
+              Text('Alvaro Fajardo Daza', style: TextStyle(color: Colors.white, fontSize: 25.0))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
